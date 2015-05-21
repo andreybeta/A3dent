203eeff3df7badbb9cd2a948a642ed67fed15308
innovusjuanpantoja <juancarlospantoja@hotmail.com>
2015-05-21 14:59:38 -0500

    Login prueba

---
+++ app/src/main/java/com/acarolabs/a3dent/Activities/Login.java
@@ -0,0 +1,29 @@
+package com.acarolabs.a3dent.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.acarolabs.a3dent.MainActivity;
+import com.acarolabs.a3dent.R;
+
+public class Login extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    public void onclick(View view){
+
+
+        Intent intent = new Intent(this,Activity2.class);
+        startActivity(intent);
+    }
+
+}
