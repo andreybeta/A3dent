68bdfe706670d25530b07a7c45e3bd9059a584a1
jorgeluis91x <jorge_luis91x@hotmail.com>
2015-05-21 18:07:37 -0500

    view page error

--- app/src/main/java/com/acarolabs/a3dent/Adapters/BeneficiosAdapter.java
+++ app/src/main/java/com/acarolabs/a3dent/Adapters/BeneficiosAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import com.acarolabs.a3dent.Models.Beneficios;
 import com.acarolabs.a3dent.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,10 +19,10 @@ import java.util.List;
  */
 public class BeneficiosAdapter extends RecyclerView.Adapter<BeneficiosAdapter.ViewHolder> {
     public static Activity activity;
-    public static List<Beneficios> listaBeneficios;
+    public static ArrayList<Beneficios> listaBeneficios;
     private int  itemLayout;
 
-    public BeneficiosAdapter(List<Beneficios> data, int itemLayout, Activity activity){
+    public BeneficiosAdapter(ArrayList<Beneficios> data, int itemLayout, Activity activity){
         super();
         this.itemLayout = itemLayout;
         this.listaBeneficios = data;
@@ -29,7 +30,7 @@ public class BeneficiosAdapter extends RecyclerView.Adapter<BeneficiosAdapter.Vi
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+    public BeneficiosAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
 
         View itemLayoutView = LayoutInflater.from(parent.getContext()).inflate(itemLayout, parent, false);
         ViewHolder viewHolder = new ViewHolder(itemLayoutView);
@@ -44,19 +45,21 @@ public class BeneficiosAdapter extends RecyclerView.Adapter<BeneficiosAdapter.Vi
 
         holder.descripcion.setText(beneficio.getBeneficios() );
         holder.puntos.setText(beneficio.getPuntos());
+        holder.itemView.setTag(beneficio);
 
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaBeneficios.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
         public ImageView imagen;
         public TextView descripcion;
         public TextView puntos;
+        public Beneficios propiedades;
 
         public ViewHolder(View itemView){
             super(itemView);
