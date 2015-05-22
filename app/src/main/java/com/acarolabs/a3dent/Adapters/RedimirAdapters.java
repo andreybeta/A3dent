3da8caf99b2b1770eaf1a97eda0c37c0f04ae956
innovusjuanpantoja <juancarlospantoja@hotmail.com>
2015-05-21 19:05:40 -0500

    vista redimir puntos

--- app/src/main/java/com/acarolabs/a3dent/Adapters/RedimirAdapters.java
+++ app/src/main/java/com/acarolabs/a3dent/Adapters/RedimirAdapters.java
@@ -2,8 +2,13 @@ package com.acarolabs.a3dent.Adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.acarolabs.a3dent.Models.Redimir;
+import com.acarolabs.a3dent.R;
 
 import java.util.ArrayList;
 
@@ -16,9 +21,6 @@ public class RedimirAdapters extends RecyclerView.Adapter<RedimirAdapters.ViewHo
     private int itemLayout;
 
 
-    public RedimirAdapters(Context context) {
-        super(context);
-    }
 
     public  RedimirAdapters(ArrayList<Redimir> redimirs,int itemLayout){
         this.redimirs = redimirs;
@@ -26,7 +28,42 @@ public class RedimirAdapters extends RecyclerView.Adapter<RedimirAdapters.ViewHo
 
     }
 
-    Ad
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(itemLayout,viewGroup,false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        Redimir redimir = redimirs.get(i);
+        viewHolder.Ponit.setText(redimir.getPoint());
+        viewHolder.aux.setText(redimir.getAux());
+        viewHolder.text.setText(redimir.getText());
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView Ponit;
+        public TextView aux;
+        public TextView text;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            Ponit = (TextView) itemView.findViewById(R.id.txtPoint);
+            aux = (TextView) itemView.findViewById(R.id.txtAux);
+            text = (TextView) itemView.findViewById(R.id.txtText);
+        }
+    }
 
 
 }
--- app/src/main/java/com/acarolabs/a3dent/Fragments/RedimirFragment.java
+++ app/src/main/java/com/acarolabs/a3dent/Fragments/RedimirFragment.java
@@ -3,11 +3,18 @@ package com.acarolabs.a3dent.Fragments;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.acarolabs.a3dent.Adapters.RedimirAdapters;
+import com.acarolabs.a3dent.Models.Redimir;
 import com.acarolabs.a3dent.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +33,25 @@ public class RedimirFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_redimir, container, false);
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayList<Redimir> redimirArrayList = new ArrayList<>();
+        Redimir redimir = new Redimir();
+        redimir.setPoint("50");
+        redimir.setAux("10");
+        redimir.setText("Redimir en ...........................");
+        redimirArrayList.add(redimir);
+        RecyclerView recyclerView = (RecyclerView) getActivity().findViewById(R.id.my_recycler_view_redimir);
+        recyclerView.setHasFixedSize( true);
+        recyclerView.setAdapter(new RedimirAdapters(redimirArrayList,R.layout.row_redimir));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+    }
+
+
 
 
 }
