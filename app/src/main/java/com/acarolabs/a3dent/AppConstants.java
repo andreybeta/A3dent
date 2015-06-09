08ee79d13305b38eb84d9643ecc7acf06a4518f7
jorgeluis91x <jorge_luis91x@hotmail.com>
2015-06-09 10:40:03 -0500

    add Constants, add ImgRewards

---
+++ app/src/main/java/com/acarolabs/a3dent/AppConstants.java
@@ -0,0 +1,11 @@
+package com.acarolabs.a3dent;
+
+/**
+ * Created by personal on 9/06/15.
+ */
+public class AppConstants {
+
+    public static final String serverUrl = "http://3dent.acarolabs.com/";
+
+
+}
